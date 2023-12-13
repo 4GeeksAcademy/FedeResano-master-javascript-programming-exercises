d9a838b88a09740a145e8f46b9a4a175112cdb66
Federico Resano <federicoresano1@gmail.com>
2023-12-13 11:55:52 -0300

    commit

--- exercises/079-findMinLengthOfThreeWords/app.js
+++ exercises/079-findMinLengthOfThreeWords/app.js
@@ -1,1 +1,4 @@
-// Write your function here
+function findMinLengthOfThreeWords(str1, str2, str3) {
+    let minLength = Math.min(str1.length, str2.length, str3.length);
+    return minLength;
+}
--- exercises/080-findMaxLengthOfThreeWords/app.js
+++ exercises/080-findMaxLengthOfThreeWords/app.js
@@ -1,1 +1,4 @@
-// Write your function here
+function findMaxLengthOfThreeWords(str1, str2, str3){
+    let maxLength = Math.max(str1.length, str2.length, str3.length);
+    return maxLength;
+}
