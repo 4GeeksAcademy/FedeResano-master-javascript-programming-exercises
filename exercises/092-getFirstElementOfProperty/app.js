c74c40fa6487c67caa948a056cb915f0447c7c1d
Federico Resano <federicoresano1@gmail.com>
2023-12-13 22:00:12 -0300

    commit

--- exercises/092-getFirstElementOfProperty/app.js
+++ exercises/092-getFirstElementOfProperty/app.js
@@ -1,1 +1,6 @@
-// Write your function here
+function getFirstElementOfProperty(obj, key){
+    if(!obj[key] || !Array.isArray(obj[key])) {
+        return undefined
+    }
+    return obj.key[0];
+}
