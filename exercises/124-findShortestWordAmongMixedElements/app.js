007cdfd2941ed2b01412bc0d4ef01eaf817c26d0
Federico Resano <federicoresano1@gmail.com>
2023-12-22 19:32:11 -0300

    commit

--- exercises/124-findShortestWordAmongMixedElements/app.js
+++ exercises/124-findShortestWordAmongMixedElements/app.js
@@ -1,6 +1,16 @@
 function findShortestWordAmongMixedElements(arr) {
-    // your code here
+    let aux = 999;
+    let shortest = "";
+    for(let i = 0; i < arr.length; i++){
+        if(typeof arr[i] == "string"){
+            if(arr[i].length < aux){
+                shortest = arr[i];
+                aux = arr[i].length;
+            } 
+        }
+    }
     
+    return shortest;
 }
 
 let output = findShortestWordAmongMixedElements([4, 'two', 2, 'three']);
