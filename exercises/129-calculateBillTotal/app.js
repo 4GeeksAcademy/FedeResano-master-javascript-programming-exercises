f10c7669240b63b10640423ed250136595d5e840
Federico Resano <federicoresano1@gmail.com>
2023-12-22 21:58:39 -0300

    commit

--- exercises/129-calculateBillTotal/app.js
+++ exercises/129-calculateBillTotal/app.js
@@ -1,6 +1,11 @@
 function calculateBillTotal(preTaxAndTipAmount) {
-    // your code here
-    
+   	// tax = 9.5% tip = 15%
+	// both applied separetly at the original amount
+	let tax = preTaxAndTipAmount * 0.095;
+
+	let tip = preTaxAndTipAmount * 0.15;
+
+	return preTaxAndTipAmount + tax + tip;
 }
 
 let output = calculateBillTotal(20);
