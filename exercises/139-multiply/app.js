bc693ef75b46ffb5411f8917426f809d1bcfdc95
Federico Resano <federicoresano1@gmail.com>
2023-12-22 23:57:50 -0300

    commit

--- exercises/139-multiply/app.js
+++ exercises/139-multiply/app.js
@@ -1,6 +1,14 @@
 function multiply(num1, num2) {
-    // your code here
-    
+ 	
+	if(num1 === 0 || num2 === 0) return 0;
+
+	let result = 0;
+
+	for(let i = 0; i < num2; i++){
+		result += num1;
+	}
+
+	return result;
 }
 
 let output = multiply(4, 7);
