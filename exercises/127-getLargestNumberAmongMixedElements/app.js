0c6b27d78470d39a843421e8ffb9d0d00abbc96c
Federico Resano <federicoresano1@gmail.com>
2023-12-22 21:21:55 -0300

    commit

--- exercises/127-getLargestNumberAmongMixedElements/app.js
+++ exercises/127-getLargestNumberAmongMixedElements/app.js
@@ -1,13 +1,13 @@
 function getLargestNumberAmongMixedElements(arr) {
-	let largestNumber = 0;
+	let largestNumber = -9999999;
 
 	for(let i = 0; i < arr.length; i++){
 		if(typeof arr[i] == "string"){
 			if(arr[i] > largestNumber) largestNumber = arr[i];
 		}
 	}
-    
-	return largestNumber;
+
+	return isPositive(largestNumber > 0)? largestNumber : 0;
 }
 
 let output = getLargestNumberAmongMixedElements([3, 'word', 5, 'up', 3, 1]);
